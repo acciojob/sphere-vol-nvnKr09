60f54becc59dac9f654e5b8419f1c7b6ebddb641
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-31 18:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     //Write your code here
   const radius = document.getElementById("radius").value;
   const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-  document.getElementById("volume").value = volume;
+  document.getElementById("volume").value = volume.toFixed(4);
 }
 document.getElementById("submit").addEventListener("click", function (e) {
 	e.preventDefault(); // Prevent form submission
