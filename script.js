d7ad1b07ee1b3b2e5b76ec839b05d0a849464992
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-31 18:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
-} 
+  const radius = document.getElementById("radius").value;
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume;
+}
+document.getElementById("submit").addEventListener("click", function (e) {
+	e.preventDefault(); // Prevent form submission
+    volume_sphere(); // Call the volume_sphere function
+});
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
